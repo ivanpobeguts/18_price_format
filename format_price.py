32e5bbcca7d6b4b20bffa3ea403a3e141e02b2a4
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-18 15:38:23 +0300

    format_price: add integer & float support tests: add new class

--- format_price.py
+++ format_price.py
@@ -1,5 +1,13 @@
 def format_price(price):
-    pass
+    if isinstance(price, float):
+        price = round(price)
+        formatted_price = '{:,}'.format(price).replace(',', ' ')
+    elif isinstance(price, int):
+        formatted_price = '{:,}'.format(price).replace(',', ' ')
+    else:
+        formatted_price = None
+    return formatted_price
+
 
 if __name__ == '__main__':
-    pass
+    print(format_price('dsf'))
--- tests.py
+++ tests.py
@@ -1,1 +1,9 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+class Format_Price_Test(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
