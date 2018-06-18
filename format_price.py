0cdeba05502944a0c6c7589c3c5f8576906a5db5
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-18 15:46:36 +0300

    fix format_price(): support all arguments' types

--- format_price.py
+++ format_price.py
@@ -1,13 +1,12 @@
 def format_price(price):
-    if isinstance(price, float):
-        price = round(price)
-        formatted_price = '{:,}'.format(price).replace(',', ' ')
-    elif isinstance(price, int):
-        formatted_price = '{:,}'.format(price).replace(',', ' ')
-    else:
-        formatted_price = None
+    try:
+        price = float(price)
+    except:
+        return None
+    price = round(price)
+    formatted_price = '{:,}'.format(price).replace(',', ' ')
     return formatted_price
 
 
 if __name__ == '__main__':
-    print(format_price('dsf'))
+    print(format_price('567839'))
